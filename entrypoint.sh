1a7bfb2e683220621f9d452d0dff7145ccc9142b
David CHERRIERE <dch@nbs-system.com>
2017-11-28 14:19:27 +0100

    Adding MySQL ACL Creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,26 @@
 #! /bin/bash
 
 usermod www-data -s /bin/bash
+/etc/init.d/apache2 start
 
 
 if [ ! -f /var/lib/mysql_data/.flag ]; then
 	chown -R www-data: /var/www
 	cp -Rv /var/lib/mysql/* /var/lib/mysql_data
 	chown -R mysql: /var/lib/mysql_data/
-	touch /var/lib/mysql_data/.flag
+
+    /etc/init.d/mysql start
+    sleep 5
+
+    mysql -u root -e "CREATE DATABASE ${DB_NAME};"
+    mysql -u root -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';"
+    mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'localhost';"
+
+    touch /var/lib/mysql_data/.flag
+
+else
+    /etc/init.d/mysql start
 fi
-
-
-a2enmod rewrite
-/etc/init.d/mysql start
-/etc/init.d/apache2 start
 
 
 /bin/bash
